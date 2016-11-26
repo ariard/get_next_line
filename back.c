fa95119b68cd286096c256b2b2e6e6ba3bb361bb
ariard@student.42.fr <ariard@student.42.fr>
2016-11-26 16:06:02 +0100

    good

---
+++ back.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ariard <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/26 14:21:54 by ariard            #+#    #+#             */
+/*   Updated: 2016/11/26 15:37:18 by ariard           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+
+int				get_next_line(const int fd, char **line)
+{
+	static char	*stock;
+	char		*tmp;
+	char		*tmp2;
+	char		*tmp3;
+	char		*string;
+	t_gnl		gnl;
+
+	if (fd < 0 || line == NULL || BUFF_SIZE <=0)
+		return (-1);
+	if (stock == NULL)
+	{
+		stock = ft_strnew(BUFF_SIZE);
+		string = ft_strnew(BUFF_SIZE);
+	}
+	else
+		string = stock;
+	ft_memset(gnl.buf, 0, BUFF_SIZE + 1);
+	while ((gnl.ret = read(fd, gnl.buf, BUFF_SIZE)) > 0)
+	{
+		tmp = string;
+		string = ft_strnew(ft_strlen(gnl.buf) + ft_strlen(tmp));
+		ft_strcpy(string, tmp);
+		ft_strcat(string, gnl.buf);
+		ft_strdel(&tmp);
+		ft_memset(gnl.buf, 0, BUFF_SIZE + 1);
+	}
+	if (*string && gnl.ret >= 0)
+	{
+		tmp2 = string;
+		tmp3 = stock;
+		while (*string != 10 && *string != 0) 
+			string++;
+		*string = '\0';
+		*line = tmp2;
+		string++;
+		stock = string;
+		return (1);
+	}
+	return (gnl.ret);
+}
--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ariard <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/21 14:17:15 by ariard            #+#    #+#             */
-/*   Updated: 2016/11/26 01:58:40 by ariard           ###   ########.fr       */
+/*   Updated: 2016/11/26 16:05:47 by ariard           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,11 @@ static size_t		ft_set_line(char *string, char **line)
 	char			*tmp;
 
 	tmp = string;
-	if (*string == 10 && *(string - 1) == 0)
-	{
-		*string = '\0';
-		return (1);
-	}
+//	if (*string == 10 && *(string - 1) == 0)
+//	{
+//		*string = '\0';
+//		return (1);
+//	}
 	while (*string != 10 && *string != 0)
 		string++;
 	*string = '\0';		
@@ -45,8 +45,6 @@ int					get_next_line(const int fd, char **line)
 	static char		*stock;
 	char			*tmp2;
 	char			*tmp;
-	char			*tmp3;
-	char			*tmp4;
 	char			*string;
 	t_gnl			gnl;
 
@@ -56,27 +54,25 @@ int					get_next_line(const int fd, char **line)
 	gnl.ret = 1;
 	if (!stock)
 		stock = ft_strnew(0);
-	tmp = ft_strdup(stock);
+	string = ft_strdup(stock);
 	while (gnl.check == NULL && gnl.ret > 0)
 	{
-		string = ft_strdup(tmp);
 		ft_memset(gnl.buf, 0, BUFF_SIZE + 1);
 		gnl.ret = read(fd, gnl.buf, BUFF_SIZE);
-		gnl.buf[BUFF_SIZE] = '\0';
-		tmp = ft_set_string(gnl.buf, string);	
+		gnl.buf[BUFF_SIZE] = '\0';	
+		string = ft_set_string(gnl.buf, string);	
 		gnl.check = ft_strchr(gnl.buf, 10);
 	}
-	if (*tmp)  
+	if (*string)  
 	{
 		*line = ft_memalloc(sizeof(char));
-		tmp2 = *line;	
-		tmp4 = tmp;
-		gnl.len = ft_set_line(tmp, line);
+		tmp2 = *line;		
+		gnl.len = ft_set_line(string, line);
 		while (gnl.len--)
-			tmp++;
-		tmp3 = stock;
-		ft_strdel(&tmp3);
-		stock = ft_strdup(tmp);
+			string++;
+		tmp = stock;	
+		stock = ft_strdup(string);
+		ft_strdel(&tmp);
 		ft_strdel(&tmp2);
 		return (1);
 	}
